9f8220d9f27dda46cec9e0ea64c031867879e10e
forestream <155133673+forestream@users.noreply.github.com>
2024-01-17 09:40:37 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.js
@@ -0,0 +1,9 @@
+function solution(num_list) {
+    var answer = [];
+    
+    for (i = 0; i < num_list.length; i++) {
+        answer[i] = num_list[num_list.length - 1 - i];    
+    }
+    
+    return answer;
+}
