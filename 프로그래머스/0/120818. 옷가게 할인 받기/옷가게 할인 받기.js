5204c5027d55c0d2dd529c6ba19b41cdf384b7bb
hanbin <155133673+forestream@users.noreply.github.com>
2024-01-23 21:58:36 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -1,7 +1,7 @@
 function solution(price) {
     var answer = 0;
     
-    answer = price >= 100000 ? (price >= 300000 ? (price >= 500000 ? Math.trunc(price * 0.8) : Math.trunc(price * 0.9)) : Math.trunc(price * 0.95)) : price;
+    answer = price >= 100000 ? price >= 300000 ? price >= 500000 ? Math.trunc(price * 0.8) : Math.trunc(price * 0.9) : Math.trunc(price * 0.95) : price;
     
     return answer;
 }
