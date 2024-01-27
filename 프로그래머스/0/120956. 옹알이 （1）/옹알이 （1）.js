d867feb29826b533bd47db97bb8890d18917eeaf
hanbin <155133673+forestream@users.noreply.github.com>
2024-01-27 17:49:23 +0900

    [level 0] Title: 옹알이 (1), Time: 0.10 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.js
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -1,18 +1,27 @@
 function solution(babbling) {
     var answer = 0;
-    let possibleWords = ['aya', 'ye', 'woo', 'ma'];
     
-    answer = babbling.reduce((acc, elem, index, array) => {
-        let letterCount = 0;
+    for (let babble of babbling) {
+        let letters = 0;
         
-        for (let possibleWord of possibleWords) {
-            if (elem.includes(possibleWord)) {
-                letterCount += possibleWord.length;
-            } 
+        if (babble.includes('aya')) {
+            letters += 3        
         }
-        console.log(elem.length === letterCount);
-        return acc + (elem.length === letterCount ? 1 : 0);
-    }, 0);
+        
+        if (babble.includes('woo')) {
+            letters += 3        
+        }
+        
+        if (babble.includes('ye')) {
+            letters += 2        
+        }
+        
+        if (babble.includes('ma')) {
+            letters += 2        
+        }
+        
+        babble.length === letters ? answer++ : 0;
+    }
     
     return answer;
 }
