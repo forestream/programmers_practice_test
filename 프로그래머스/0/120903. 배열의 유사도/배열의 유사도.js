f4a903f5a6bac06ecf1d8a6566f1c11cbdfd920e
hanbin <155133673+forestream@users.noreply.github.com>
2024-01-31 21:26:52 +0900

    [level 0] Title: 배열의 유사도, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,7 +1,3 @@
 function solution(s1, s2) {
-    return s1.reduce((acc, word) => {
-        s2.includes(word) ? ++acc : 0;
-        console.log(acc);
-        return acc;
-    }, 0);
+    return s1.reduce((acc, word) => s2.includes(word) ? ++acc : acc, 0);
 }
