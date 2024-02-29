16b294dc72bfa2c32f9ada76391c4837683e2637
hanbin <155133673+forestream@users.noreply.github.com>
2024-02-29 10:13:24 +0900

    [level 0] Title: 배열 비교하기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.js
@@ -0,0 +1,11 @@
+function solution(arr1, arr2) {
+    if (arr1.length === arr2.length) {
+        let sum1 = arr1.reduce((acc, num) => acc + num, 0);
+        let sum2 = arr2.reduce((acc, num) => acc + num, 0);
+        
+        if (sum1 === sum2) return 0;
+        if (sum1 > sum2) return 1;
+        else return -1;
+    }
+    return arr1.length > arr2.length ? 1 : -1;
+}
