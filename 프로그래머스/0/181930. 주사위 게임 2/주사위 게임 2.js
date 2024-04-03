577d214f101c2a96d648184e2926d77d463a006a
hanbin <155133673+forestream@users.noreply.github.com>
2024-04-03 20:33:07 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
@@ -0,0 +1,7 @@
+function solution(a, b, c) {
+    let answer = 0;
+    if (a !== b && b !== c && c !== a) return a + b + c;
+    if (a === b && b === c) return (a + b + c) * (a**2 + b**2 + c**2) * (a**3 + b**3 + c**3);
+    return (a + b + c) * (a**2 + b**2 + c**2);
+    
+}
