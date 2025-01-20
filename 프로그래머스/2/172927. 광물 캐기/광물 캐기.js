d516b824b20d68a35b83816d3896e2ab75f9c5ce
hanbin <155133673+forestream@users.noreply.github.com>
2025-01-20 15:18:57 +0900

    [level 2] Title: 광물 캐기, Time: 24.66 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.js
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.js
@@ -1,106 +1,179 @@
-const mineralStrength = {
+function React() {
+    let renderCount = 0;
+    
+    let renderer;
+    const rendererQueue = [];
+    const enqueueRenderer = (renderer) => rendererQueue.push(renderer);
+    const dequeueRenderer = () => rendererQueue.shift();
+    
+    const states = [];
+    let stateIndex = 0;
+
+    function useState(initState) {
+        const currentIndex = stateIndex++;
+        
+        if (renderCount === 0) states.push(typeof initState === 'function' ? initState() : initState);
+        
+        const setState = (newState) => {
+            states[currentIndex] = typeof newState === 'function' ? newState(states[currentIndex]) : newState;
+            
+            enqueueRenderer(renderer);
+        }
+        
+        return [states[currentIndex], setState];
+    }    
+    
+    const effects = [];
+    let effectIndex = 0;
+    const effectsQueue = [];
+    const enqueueEffect = (effect) => effectsQueue.push(effect);
+    const dequeueEffect = () => effectsQueue.shift();
+    
+    function useEffect(callback, deps) {
+        const currentIndex = effectIndex++;
+        
+        if (renderCount === 0) {
+            effects[currentIndex] = [callback, deps];
+            enqueueEffect(effects[currentIndex][0]);
+            return;
+        }
+        
+        const prevDeps = effects[currentIndex][1];
+        const hasSameDeps = Array.isArray(prevDeps) && prevDeps.every((prevDep, i) => prevDep === deps[i]);
+        
+        if (hasSameDeps) return;
+        
+        effects[currentIndex] = [callback, deps];
+        enqueueEffect(effects[currentIndex][0]);
+    }
+    
+    function render(newRenderer) {
+        if (!renderer) {
+            renderer = () => {
+                console.log(states);
+                stateIndex = 0;
+                effectIndex = 0;
+                newRenderer();
+            }
+        }
+        
+        enqueueRenderer(renderer);
+        
+        while (rendererQueue.length) {
+            dequeueRenderer()();
+            
+            while (effectsQueue.length) {
+                dequeueEffect()();
+            }
+            
+            renderCount++;
+        }
+        
+        return states;
+    }
+    
+    return { render, useState, useEffect };
+}
+
+const MINERAL_STRENGTH = {
     stone: [1,1,1],
     iron: [1,1,5],
     diamond: [1,5,25]
 }
 
-const pickIndex = {
-    diamond: 0,
-    iron: 1,
-    stone: 2,
-}
-
 function getRequiredEnergy(pickType, minerals) {
-    return minerals.reduce((acc, mineral) => acc + mineralStrength[mineral][pickType], 0)
+    return minerals.reduce((acc, mineral) => acc + MINERAL_STRENGTH[mineral][pickType], 0)
 }
 
-function useState(initState) {
-    let state = initState;
+function main(initPicks, initMinerals, useState, useEffect) {
+    const totalPicks = initPicks.reduce((acc, pick) => acc + pick * 5, 0);
+    const maxMinerals = initMinerals.slice(0, totalPicks);
+    const [minEnergy, setMinEnergy] = useState(0);
+    const [picks, setPicks] = useState([...initPicks]);
+    const [chunks, setChunks] = useState(
+        Array.from({
+            length: Math.ceil(maxMinerals.length / 5),
+        }, (_, i) => ({
+            id: i,
+            requiredEnergy: 0,
+            minerals: []
+        }))
+    );
     
-    const get = () => state;
+    useEffect(() => {
+        maxMinerals.forEach((mineral, i) => {
+            const chunkIndex = Math.floor(i / 5);
+
+            setChunks((prev) => prev.map((chunk) => {
+                if (chunk.id !== chunkIndex) return chunk;
+
+                return {
+                    ...chunk,
+                    requiredEnergy: chunk.requiredEnergy + getRequiredEnergy(2, [mineral]),
+                    minerals: [...chunk.minerals, mineral]
+                }
+            })); 
+        });
     
-    const set = (newState) => state = typeof newState === 'function' ? newState(state) : newState;
+        setChunks((prev) => prev.sort((a, b) => b.requiredEnergy - a.requiredEnergy));
+    }, []);
     
-    return [get, set];
+    useEffect(() => {
+        chunks.forEach((chunk) => {
+            if (chunk.requiredEnergy > 25 || chunk.requiredEnergy === 25 && chunk.minerals.length === 1) {
+                for (let i of [0,1,2]) {
+                    if (picks[i] > 0) {
+                        setPicks((prevPicks) => [
+                            ...prevPicks.slice(0, i), 
+                            --picks[i], 
+                            ...prevPicks.slice(i + 1, prevPicks.length)
+                        ]);
+
+                        setMinEnergy((prev) => prev + getRequiredEnergy(i, chunk.minerals));
+
+                        break;
+                    }
+                }
+            } else if (chunk.requiredEnergy > 5 || chunk.requiredEnergy === 5 && chunk.minerals.length === 1) {
+                for (let i of [1,0,2]) {
+                    if (picks[i] > 0) {
+                        setPicks((prevPicks) => [
+                            ...prevPicks.slice(0, i), 
+                            --picks[i], 
+                            ...prevPicks.slice(i + 1, prevPicks.length)
+                        ]);
+
+                        setMinEnergy((prev) => prev + getRequiredEnergy(i, chunk.minerals));
+
+                        break;
+                    }
+                }
+            } else if (chunk.requiredEnergy > 0) {
+                for (let i of [2,1,0]) {
+                    if (picks[i] > 0) {
+                        setPicks((prevPicks) => [
+                            ...prevPicks.slice(0, i), 
+                            --picks[i], 
+                            ...prevPicks.slice(i + 1, prevPicks.length)
+                        ]);
+
+                        setMinEnergy((prev) => prev + getRequiredEnergy(i, chunk.minerals));
+
+                        break;
+                    }
+                }
+
+            }
+        });
+    }, [chunks])
+    
+    return minEnergy;
 }
 
 function solution(picks, minerals) {
-    const accPicks = picks.reduce((acc, pick) => acc + pick * 5, 0);
-    minerals = minerals.slice(0, accPicks);
-    const [getMinEnergy, setMinEnergy] = useState(0);
-    const [getPicks, setPicks] = useState([...picks]);
-    const [getChunks, setChunks] = useState(Array.from({
-        length: Math.ceil(minerals.length / 5),
-    }, (_, i) => ({
-        id: i,
-        requiredEnergy: 0,
-        minerals: []
-    })));
+    const {useState, render, useEffect} = React();
     
-    minerals.forEach((mineral, i) => {
-        const chunkIndex = Math.floor(i / 5);
-        
-        setChunks((prev) => prev.map((chunk) => {
-            if (chunk.id !== chunkIndex) return chunk;
-            
-            return {
-                ...chunk,
-                requiredEnergy: chunk.requiredEnergy + getRequiredEnergy(2, [mineral]),
-                minerals: [...chunk.minerals, mineral]
-            }
-        })); 
-    });
+    const answer = render(() => main(picks, minerals, useState, useEffect));
     
-    setChunks((prev) => {
-        return prev.sort((a, b) => b.requiredEnergy - a.requiredEnergy)
-    });
-    
-    getChunks().forEach((chunk) => {
-        if (chunk.requiredEnergy > 25 || chunk.requiredEnergy === 25 && chunk.minerals.length === 1) {
-            for (let i of [0,1,2]) {
-                if (getPicks()[i] > 0) {
-                    setPicks((prevPicks) => prevPicks.map((pick, pickIndex) => {
-                        if (i !== pickIndex) return pick;
-
-                        return --pick;
-                    }));
-
-                    setMinEnergy((prev) => prev + getRequiredEnergy(i, chunk.minerals));
-
-                    break;
-                }
-            }
-        } else if (chunk.requiredEnergy > 5 || chunk.requiredEnergy === 5 && chunk.minerals.length === 1) {
-            for (let i of [1,0,2]) {
-                if (getPicks()[i] > 0) {
-                    setPicks((prevPicks) => prevPicks.map((pick, pickIndex) => {
-                        if (i !== pickIndex) return pick;
-
-                        return --pick;
-                    }));
-
-                    setMinEnergy((prev) => prev + getRequiredEnergy(i, chunk.minerals));
-
-                    break;
-                }
-            }
-        } else {
-            for (let i of [2,1,0]) {
-                if (getPicks()[i] > 0) {
-                    setPicks((prevPicks) => prevPicks.map((pick, pickIndex) => {
-                        if (i !== pickIndex) return pick;
-
-                        return --pick;
-                    }));
-
-                    setMinEnergy((prev) => prev + getRequiredEnergy(i, chunk.minerals));
-
-                    break;
-                }
-            }
-            
-        }
-    })
-    
-    return getMinEnergy();
+    return answer[0];
 }
